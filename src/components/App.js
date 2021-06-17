fa3a7dd2ea05f984516b72aae7529404e35f3108
Acla Putra <muhamadacla22@gmail.com>
2021-06-18 00:10:13 +0700

    added App.js inside components folder

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h2 className="text-secondary text-center mt-4">This is App.js</h2>
+        </div>
+    )
+}
+export default App
