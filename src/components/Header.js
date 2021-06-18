89edd6e7fdb2fd8b80f36977ef4667adfe38bb37
Acla Putra <muhamadacla22@gmail.com>
2021-06-19 01:59:17 +0700

    added Header components layout

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <p className="navbar-brand">shopku</p>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
