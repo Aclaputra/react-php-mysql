2c117d9907aafd9740df2f13ddce9902e4aeec19
Acla Putra <muhamadacla22@gmail.com>
2021-09-20 03:26:54 +0700

    modified src/axios/Api.js

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost/tokoAPI/'
+    // locate your own connections/ambildata.php
+    baseURL: 'http://localhost/latihan-react/react-php-mysql/connections/ambildata.php'
 });
