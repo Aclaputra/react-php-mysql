42540bc287d39cea6fe4c44b64fbf1f835b8fbe8
Acla Putra <muhamadacla22@gmail.com>
2021-09-20 03:27:04 +0700

    modified src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Router} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 import Header from './Header'
 import DaftarProduk from './DaftarProduk'
