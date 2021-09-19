67057bcefcf6cd3d83993409036f0d9ebc99943e
Acla Putra <muhamadacla22@gmail.com>
2021-09-20 05:06:16 +0700

    added nav link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Header() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <p className="navbar-brand">shopku</p>
+
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/add" className="nav-link">Add Produk</Link>
+                    </li>
+                </ul>
             </nav>
         </div>
     )
